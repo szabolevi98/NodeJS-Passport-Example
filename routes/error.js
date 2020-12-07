e28587b2856c944000d72df11665444fa76605ac
Levente <Levente@DESKTOP-JPGKIQC>
2020-12-08 00:04:30 +0100

    2020.12.08.

--- routes/error.js
+++ routes/error.js
@@ -3,7 +3,9 @@ const router = require('express').Router();
 
 //Route error
 router.get('/', (req, res) => {
-  res.render('error');
+  res.render('error', {
+    title: 'Hiba'
+  });
 });
 
 module.exports = router;
