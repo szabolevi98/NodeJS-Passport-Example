5c698f039d91b937648db521b504a5fdf57610dc
szabolevi98 <46320470+szabolevi98@users.noreply.github.com>
2020-12-08 20:42:26 +0100

    2020.12.08.

--- routes/error.js
+++ routes/error.js
@@ -5,6 +5,7 @@ const router = require('express').Router();
 router.get('/', (req, res) => {
   res.render('error', {
     title: 'Hiba',
+    atError: true,
     route: JSON.stringify(req.originalUrl)
   });
 });
